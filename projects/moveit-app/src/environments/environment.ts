7aa556c2004e50e3ac6dc5657a705e1cafa64f70
Dzheylyan Kodzhabash <dkodzhabash@gmail.com>
2022-02-27 16:34:54 +0200

    [MA-0009] Add `baseUrl` and `ApiKey` to the `environment` file
    - *no good place for apiKey*

--- projects/moveit-app/src/environments/environment.ts
+++ projects/moveit-app/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://api.themoviedb.org/3',
+  apiKey: 'fbfbed5f5f47d3442d591bf1738745e3'
 };
 
 /*
